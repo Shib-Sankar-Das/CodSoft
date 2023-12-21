02c5817c0adc6dc4bf5122d1c28d1ec905e3c1b8
Shib Sankar Das <shibsankardas721@gmail.com>
2023-12-21 19:50:28 +0530

    enable keyboard in calculator

--- Calculator/script.js
+++ Calculator/script.js
@@ -3,30 +3,45 @@ let buttons = document.querySelectorAll('button');
 
 let string = "";
 let arr = Array.from(buttons);
+
+// Function to update input and string
+function updateInputAndString(newValue) {
+    string = newValue;
+    input.value = string;
+}
+
 arr.forEach(button => {
-    button.addEventListener('click',(e) =>{
-        if(e.target.innerHTML == '=')
-        {
-            string = eval(string)
-            input.value = string;
-        }
+    button.addEventListener('click', (e) => {
+        handleButtonClick(e.target.innerHTML);
+    });
+});
 
-        else if(e.target.innerHTML == 'AC')
-        {
-            string = "";
-            input.value = string;
-        }
+document.addEventListener('keydown', (e) => {
+    const keyPressed = e.key;
 
-        else if(e.target.innerHTML == 'DEL')
-        {
-            string = string.substring(0,string.length-1);
-            input.value = string;
-        }
-        
-        else
-        {
-            string +=e.target.innerHTML;
-            input.value = string;
-        }
-    })
-})
+    // Check if the pressed key is a number, operator, or Enter
+    const isNumber = /[0-9]/.test(keyPressed);
+    const isOperator = /[+\-*/]/.test(keyPressed);
+    const isEqual = (keyPressed === '=' || e.code === 'Enter');
+    const isDelete = (keyPressed === 'Delete' || keyPressed === 'Backspace');
+
+    if (isNumber || isOperator) {
+        handleButtonClick(keyPressed);
+    } else if (isEqual) {
+        updateInputAndString(eval(string));
+    } else if (isDelete) {
+        updateInputAndString(string.slice(0, -1));
+    }
+});
+
+function handleButtonClick(buttonValue) {
+    if (buttonValue === '=') {
+        updateInputAndString(eval(string));
+    } else if (buttonValue === 'AC') {
+        updateInputAndString("");
+    } else if (buttonValue === 'DEL') {
+        updateInputAndString(string.slice(0, -1));
+    } else {
+        updateInputAndString(string + buttonValue);
+    }
+}
