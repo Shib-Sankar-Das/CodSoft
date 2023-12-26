f2211516e291fbe4c5a4b93ffdbc80dc242bb5f2
Shib Sankar Das <shibsankardas721@gmail.com>
2023-12-27 05:16:11 +0530

    Modal in Portfolio

--- Portfolio/script.js
+++ Portfolio/script.js
@@ -24,3 +24,25 @@ menuIcon.onclick = () => {
     menuIcon.classList.toggle('bx-x');
     navbar.classList.toggle('active');
 }
+
+
+function openModal(modalId) {
+    var modal = document.getElementById(modalId);
+    modal.style.display = "block";
+}
+
+function closeModal(modalId) {
+    var modal = document.getElementById(modalId);
+    modal.style.display = "none";
+}
+
+// Close the modal if the user clicks outside the modal content
+window.onclick = function(event) {
+    var modals = document.querySelectorAll('.modal');
+    modals.forEach(function(modal) {
+        if (event.target == modal) {
+            modal.style.display = "none";
+        }
+    });
+}
+
