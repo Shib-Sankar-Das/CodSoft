39cda6457288555ef02a4c88f6173beee4e11b32
Shib Sankar Das <shibsankardas721@gmail.com>
2023-12-29 02:51:12 +0530

    modal & typing animation responsive error fixed

--- Portfolio/script.js
+++ Portfolio/script.js
@@ -46,3 +46,21 @@ window.onclick = function(event) {
     });
 }
 
+
+var acc = document.getElementsByClassName("accordion");
+var i;
+
+for (i = 0; i < acc.length; i++) {
+  acc[i].addEventListener("click", function () {
+    this.classList.toggle("active1");
+    this.parentElement.classList.toggle("active1");
+
+    var pannel = this.nextElementSibling;
+
+    if (pannel.style.display === "block") {
+      pannel.style.display = "none";
+    } else {
+      pannel.style.display = "block";
+    }
+  });
+}
